96196e93b7aec3573068a4fc4b7e2737f81169fc
BeardedPlatypus <mwtegelaers@gmail.com>
2021-09-15 11:32:44 +0200

    Update react-app/src/App.js with Unity element

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl"
+
+
+const unityContext = new UnityContext({
+  loaderUrl: "unity/web-study-build.loader.js",
+  dataUrl: "unity/web-study-build.data",
+  frameworkUrl: "unity/web-study-build.framework.js",
+  codeUrl: "unity/web-study-build.wasm",
+});
+
+const unityStyle = {
+  height: "100%",
+  width: 950,
+  border: "2px solid black",
+  background: "grey",
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+          <h1>
+            Unity Web Study #1.
+          </h1>
+        </div>
+        <div style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+          <Unity
+            unityContext={unityContext}
+            style={unityStyle}
+          />
+        </div>
       </header>
     </div>
   );
